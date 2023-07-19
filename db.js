d3117c8027cc55e3f1a28b7cdfe622150428c984
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-07-19 09:25:27 +0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+// const DB_URI = process.env.DB_URI;
+const DB_URI = 'mongodb+srv://art777oleg:y9IZuTf1EQjXgkE5@cluster0.1z4hvbl.mongodb.net/Contacts?retryWrites=true&w=majority';
+// const DB_URI = process.env.DB_URI || 'mongodb+srv://art777oleg:y9IZuTf1EQjXgkE5@cluster0.1z4hvbl.mongodb.net/Contacts?retryWrites=true&w=majority';
+
+async function run()
+{
+    try
+    {
+        await mongoose.connect(DB_URI);
+
+        console.log("Database connection successful!");
+    }
+    catch (error)
+    {
+        console.error(error);
+        
+        process.exit(1);
+    }
+};
+run().catch(console.error);
