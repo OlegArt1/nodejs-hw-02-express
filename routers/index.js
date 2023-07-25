5c205b59174933d3731b9fc1b518f3051f6cb2d3
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-07-25 10:22:35 +0300

    Updated code

--- routers/index.js
+++ routers/index.js
@@ -4,14 +4,12 @@ const auth = require("../middleware/auth");
 
 const authRoutes = require("./auth");
 const userRoutes = require("./users");
-const bookRoutes = require("./books");
 const contactsRoutes = require("./contacts");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
-router.use("/books", auth, bookRoutes);
 router.use("/contacts", auth, contactsRoutes);
 
 module.exports = router;
