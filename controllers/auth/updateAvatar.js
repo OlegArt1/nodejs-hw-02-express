3af35e60c999aa56d81201eb6b55f0c1429532af
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-07-26 16:13:55 +0300

    Update updateAvatar.js

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -27,34 +27,17 @@ async function updateAvatar (req, res, next)
     
         const avatarUrl = path.join("avatars", filename);
     
-        const avatar = await User.findByIdAndUpdate(id, { avatarURL: avatarUrl }, { new: true });
+        await User.findByIdAndUpdate(id, { avatarURL: avatarUrl }, { new: true });
     
-        if (avatar === null)
-        {
-            console.log("Unauthorized!");
-
-            res.status(401).json({
-                status: "Unauthorized",
-                code: 401,
-                contentType: "application/json",
-                responseBody:
-                {
-                    message: "Not authorized"
-                }
-            });
-        }
-        else
-        {
-            res.status(200).json({
-                status: "OK",
-                code: 200,
-                contentType: "application/json",
-                responseBody:
-                {
-                    avatarURL: avatarUrl
-                }
-            });
-        }
+        res.status(200).json({
+            status: "OK",
+            code: 200,
+            contentType: "application/json",
+            responseBody:
+            {
+                avatarURL: avatarUrl
+            }
+        });
     }
     catch (error)
     {
